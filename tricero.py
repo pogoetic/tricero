573378f011ad8c08d8f98589ef02a2a131e79b17
pogoetic <pogster@gmail.com>
2018-05-13 11:20:14 -0400

    fixed time conversion

--- tricero.py
+++ tricero.py
@@ -33,7 +33,7 @@ def dbprocess(path):
      
 def convert_my_iso_8601(iso_8601, tz_info):
     assert iso_8601[-1] == 'Z'
-    iso_8601 = iso_8601[:-1] + '000'
+    iso_8601 = iso_8601[0:-2]
     iso_8601_dt = dt.datetime.strptime(iso_8601, '%Y-%m-%dT%H:%M:%S.%f')
     return iso_8601_dt.replace(tzinfo=timezone('UTC')).astimezone(tz_info)
 
